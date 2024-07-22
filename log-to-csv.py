712fd42a8c953d96c1e8ef0301d322c6d29d75d5
John Sturdy <jcg.sturdy@gmail.com>
2024-07-22 20:38:23 +0100

    handle the whole file, taking just the first copy of everything

--- log-to-csv.py
+++ log-to-csv.py
@@ -5,10 +5,15 @@ import re
 outstream = None
 
 with open("pieces.log") as instream:
+    groups_seen = set()
     while line := instream.readline():
         line = line.removeprefix("ECHO: ")
         if m := re.match('"start of ([^"]+)', line):
-            outstream = open(m.group(1).replace(' ', '-')+"-pieces.csv", 'w')
+            groupname = m.group(1).replace(' ', '-')
+            if groupname in groups_seen:
+                break
+            groups_seen.add(groupname)
+            outstream = open(groupname+"-pieces.csv", 'w')
         elif line.startswith("end of"):
             outstream.close()
             outstream = None
